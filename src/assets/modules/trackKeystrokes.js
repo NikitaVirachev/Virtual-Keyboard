9e545645d0ef53e71bed3bb8ecae70cccb7938e6
NikitaVirachev <guardsolder@gmail.com>
2022-05-07 18:40:55 +0300

    feat: implement the functionality of the Enter button

--- src/assets/modules/trackKeystrokes.js
+++ src/assets/modules/trackKeystrokes.js
@@ -6,17 +6,26 @@ export function trackKeystrokes() {
     key.classList.add('pressed');
 
     let textarea = document.getElementById('textarea');
+    let selStart = textarea.selectionStart;
 
     if (key.classList[1] === 'standard') {
       textarea.value += event.key;
     } else if (key.classList[1] === 'backspace') {
-      textarea.value = textarea.value.slice(0, -1);
+      textarea.value = textarea.value.slice(0, selStart - 1)
+        + textarea.value.slice(selStart);
+      textarea.selectionStart = selStart - 1;
+      textarea.selectionEnd = selStart - 1;
     } else if (key.classList[1] === 'del') {
-      let selStart = textarea.selectionStart;
       textarea.value = textarea.value.slice(0, selStart)
         + textarea.value.slice(selStart + 1);
       textarea.selectionStart = selStart;
       textarea.selectionEnd = selStart;
+    } else if (key.classList[1] === 'enter') {
+      textarea.value = textarea.value.slice(0, selStart)
+        + '\n'
+        + textarea.value.slice(selStart);
+      textarea.selectionStart = selStart + 1;
+      textarea.selectionEnd = selStart + 1;
     }
   });
 
