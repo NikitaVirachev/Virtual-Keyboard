4fbf2dce8571f7b0a56d888e2e39cbe270f540ae
NikitaVirachev <guardsolder@gmail.com>
2022-05-09 14:00:01 +0300

    feat: implement space button functionality on mouse click

--- src/assets/modules/trackMouseClick.js
+++ src/assets/modules/trackMouseClick.js
@@ -106,6 +106,12 @@ export function mouseDown(event) {
       + textarea.value.slice(selStart);
     textarea.selectionStart = selStart + 1;
     textarea.selectionEnd = selStart + 1;
+  } else if (key.classList[1] === 'space') {
+    textarea.value = textarea.value.slice(0, selStart)
+      + ' '
+      + textarea.value.slice(selStart);
+    textarea.selectionStart = selStart + 1;
+    textarea.selectionEnd = selStart + 1;
   }
 }
 
