05509f0c517e9b8f985557c0d07d7a31d595d9b1
NikitaVirachev <guardsolder@gmail.com>
2022-05-09 14:12:45 +0300

    feat: implement shift button functionality on mouse click

--- src/assets/modules/trackMouseClick.js
+++ src/assets/modules/trackMouseClick.js
@@ -45,6 +45,7 @@ export function mouseDown(event) {
   let textarea = document.getElementById('textarea');
   let selStart = textarea.selectionStart;
   let selEnd = textarea.selectionEnd;
+  let allCharacterKeys = document.querySelectorAll('.standard');
 
   if (key.classList[1] === 'standard') {
     textarea.value = textarea.value.slice(0, selStart)
@@ -118,6 +119,65 @@ export function mouseDown(event) {
       + textarea.value.slice(selStart);
     textarea.selectionStart = selStart + 1;
     textarea.selectionEnd = selStart + 1;
+  } else if ((key.classList[1] === 'shift-left') || (key.classList[1] === 'shift-right')) {
+    state.setShift(true);
+
+    if ((key.classList[1] === 'shift-left') && state.getAlt()) {
+      if (sessionStorage.getItem('layout') === 'eng') {
+        sessionStorage.setItem('layout', 'rus');
+
+        allCharacterKeys.forEach(element => {
+          let keyEng = element.querySelector('.key__eng');
+          let keyRus = element.querySelector('.key__rus');
+
+          keyEng.classList.add('hidden');
+          keyRus.classList.remove('hidden');
+        });
+      } else {
+        sessionStorage.setItem('layout', 'eng');
+
+        allCharacterKeys.forEach(element => {
+          let keyEng = element.querySelector('.key__eng');
+          let keyRus = element.querySelector('.key__rus');
+
+          keyRus.classList.add('hidden');
+          keyEng.classList.remove('hidden');
+        });
+      }
+    }
+
+    allCharacterKeys.forEach(element => {
+      let keyEng = element.querySelector('.key__eng');
+      let keyRus = element.querySelector('.key__rus');
+
+      if (keyEng.matches('.hidden')) {
+        let ruCaps = keyRus.querySelector('.capsLock');
+        let ruCaseDown = keyRus.querySelector('.caseDown');
+        let ruCaseUp = keyRus.querySelector('.caseUp');
+        let ruShiftCaps = keyRus.querySelector('.shiftCaps');
+
+        if (state.getCaps()) {
+          ruCaps.classList.add('hidden');
+          ruShiftCaps.classList.remove('hidden');
+        } else {
+          ruCaseDown.classList.add('hidden');
+          ruCaseUp.classList.remove('hidden');
+        }
+      } else {
+        let engCaps = keyEng.querySelector('.capsLock');
+        let engCaseDown = keyEng.querySelector('.caseDown');
+        let engCaseUp = keyEng.querySelector('.caseUp');
+        let engShiftCaps = keyEng.querySelector('.shiftCaps');
+
+        if (state.getCaps()) {
+          engCaps.classList.add('hidden');
+          engShiftCaps.classList.remove('hidden');
+        } else {
+          engCaseDown.classList.add('hidden');
+          engCaseUp.classList.remove('hidden');
+        }
+      }
+    });
   }
 }
 
@@ -128,5 +188,44 @@ export function mouseUp() {
     key.classList.remove('pressed');
     key.classList.add('unpressed');
     state.setCurrentKeyId(null);
+  }
+
+  let allCharacterKeys = document.querySelectorAll('.standard');
+
+  if ((key.classList[1] === 'shift-left') || (key.classList[1] === 'shift-right')) {
+    state.setShift(false);
+
+    allCharacterKeys.forEach(element => {
+      let keyEng = element.querySelector('.key__eng');
+      let keyRus = element.querySelector('.key__rus');
+
+      if (keyEng.matches('.hidden')) {
+        let ruCaps = keyRus.querySelector('.capsLock');
+        let ruCaseDown = keyRus.querySelector('.caseDown');
+        let ruCaseUp = keyRus.querySelector('.caseUp');
+        let ruShiftCaps = keyRus.querySelector('.shiftCaps');
+
+        if (state.getCaps()) {
+          ruShiftCaps.classList.add('hidden');
+          ruCaps.classList.remove('hidden');
+        } else {
+          ruCaseUp.classList.add('hidden');
+          ruCaseDown.classList.remove('hidden');
+        }
+      } else {
+        let engCaps = keyEng.querySelector('.capsLock');
+        let engCaseDown = keyEng.querySelector('.caseDown');
+        let engCaseUp = keyEng.querySelector('.caseUp');
+        let engShiftCaps = keyEng.querySelector('.shiftCaps');
+
+        if (state.getCaps()) {
+          engShiftCaps.classList.add('hidden');
+          engCaps.classList.remove('hidden');
+        } else {
+          engCaseUp.classList.add('hidden');
+          engCaseDown.classList.remove('hidden');
+        }
+      }
+    });
   }
 }
